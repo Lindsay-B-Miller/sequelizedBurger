f5d3c020134362e79ca92e19e41824c7bf0ad63f
Lindsay <lindsay.lbm@gmail.com>
2019-10-30 15:52:35 -0500

    added model

--- models/burger.js
+++ models/burger.js
@@ -1,15 +1,34 @@
-var orm = require("../config/orm");
+module.exports = function (sequelize, DataTypes) {
+  var Burger = sequelize.define("Burger", {
+    burger_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: true
+      }
+    },
+    devoured: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+      validate: {
+        notNull: true
+      }
+    }
+  });
+  return Burger;
+};
 
 var burger = {
-  all: function(cb) {
-    orm.all("burgers", function(res) {
+  all: function (cb) {
+    orm.all("burgers", function (res) {
       cb(res);
     });
   },
-  create: function(name, cb) {
+  create: function (name, cb) {
     orm.create("burgers", ["burger_name", "devoured"], [name, false], cb);
   },
-  update: function(id, cb) {
+  update: function (id, cb) {
     var condition = "id=" + id;
     orm.update("burgers", {
       devoured: true
@@ -17,4 +36,4 @@ var burger = {
   }
 };
 
-module.exports = burger;
+
